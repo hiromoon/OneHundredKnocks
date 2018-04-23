64d44b6f0bdefeda329b5b4f2c8968b5a933d14d
hiromoon <hiromoon0428@gmail.com>
2018-04-24 02:30:34 +0900

    [add] 4本目を追加

--- elixir/lib/chaper_one.ex
+++ elixir/lib/chaper_one.ex
@@ -23,9 +23,44 @@ defmodule ChapterOne do
     |> Enum.join
   end
 
-  def pi(str) do
+  defp parseToWord(str) do
     Regex.replace(~r/[\.,]/, str, "")
     |> String.split
+  end
+
+  def pi(str) do
+    str
+    |> parseToWord
     |> Enum.map(&String.length/1)
+  end
+
+  defp toShortKey(map) do
+    Enum.reduce(map, %{},
+                fn ({key, val}, acc) ->
+                  case val do
+                    1 -> Map.put(acc, String.first(key), val)
+                    5 -> Map.put(acc, String.first(key), val)
+                    6 -> Map.put(acc, String.first(key), val)
+                    7 -> Map.put(acc, String.first(key), val)
+                    8 -> Map.put(acc, String.first(key), val)
+                    9 -> Map.put(acc, String.first(key), val)
+                    15 -> Map.put(acc, String.first(key), val)
+                    16 -> Map.put(acc, String.first(key), val)
+                    19 -> Map.put(acc, String.first(key), val)
+                    _ -> Map.put(acc, String.slice(key, 0..1), val)
+                  end
+                end)
+  end
+
+  defp listToMap(list) do
+    Enum.zip(1..length(list), list)
+    |> List.foldl(%{}, fn({index, key}, acc) -> Map.put(acc, key, index) end)
+  end
+
+  def elements(str) do
+    str
+    |> parseToWord
+    |> listToMap
+    |> toShortKey
   end
 end
--- elixir/test/chapter_1_test.exs
+++ elixir/test/chapter_1_test.exs
@@ -16,4 +16,31 @@ defmodule ChapterOneTest do
   test "03_Pi" do
     assert ChapterOne.pi("Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.") == [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
   end
+
+  test "04_elements" do
+    target = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+    ans = %{
+      "H" => 1,
+      "He" => 2,
+      "Li" => 3,
+      "Be" => 4,
+      "B" => 5,
+      "C" => 6,
+      "N" => 7,
+      "O" => 8,
+      "F" => 9,
+      "Ne" => 10,
+      "Na" => 11,
+      "Mi" => 12,
+      "Al" => 13,
+      "Si" => 14,
+      "P" => 15,
+      "S" => 16,
+      "Cl" => 17,
+      "Ar" => 18,
+      "K" => 19,
+      "Ca" => 20
+    }
+    assert ChapterOne.elements(target) == ans
+  end
 end
